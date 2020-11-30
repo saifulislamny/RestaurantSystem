c7554ad71296fc6f4a8354ac52656e0957ddb2ff
danlichter096 <52501911+danlichter096@users.noreply.github.com>
2020-11-30 14:54:20 -0500

    Update surfer_operations.py

--- scripts/surfer_operations.py
+++ scripts/surfer_operations.py
@@ -1,11 +1,23 @@
 def surfer_apply_for_registered_customer(username, password, deposit): # TODO: Daniel, implement this function
-    ''' 
+    '''
     username: username that surfer requests to have (not guaranteed to meet conditions)
     password: password that surfer requests to have (not guaranteed to meet conditions)
     deposit: deposit that surfer requests to set (not guaranteed to meet conditions)
-    Output: Returns true/false if surfer's requested username, password, and deposit information is successfully added to CustomerRegistrations table 
+    Output: Returns true/false if surfer's requested username, password, and deposit information is successfully added to CustomerRegistrations table
     '''
     # if username does not match conditions in Accounts table, then return false
+    cnx = connect_to_db()
+    cur = get_cursor(cnx)
+    if(len(username)>15):
+        return False
     # if password does not match conditions in Accounts table, then return false
+    elif(len(password)>15):
+        return False
     # if deposit is not a positive number, then return false
+    elif(deposit < 0):
+        return False
     # otherwise perform the operations and return true
+    else:
+        cur.execute("INSERT INTO CustomerRegistrations(cust_username, password, amt_of_deposit) VALUES (%s,%s,%s)", (username, password, deposit))
+        save_db_changes(cur,cnx)
+        return True
