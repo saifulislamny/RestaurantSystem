f42cd2bda92111ad9d9465d3486d556583da9d5f
danlichter096 <52501911+danlichter096@users.noreply.github.com>
2020-11-30 14:51:52 -0500

    Update discussion_board_operations.py

--- scripts/discussion_board_operations.py
+++ scripts/discussion_board_operations.py
@@ -1,2 +1,21 @@
-def view_discussion_boards(): # TODO: Daniel, implement this function
+def view_discussion_boards(): 
     ''' Shows all rows in DiscussionBoardForDishes, DiscussionBoardForChefs, and DiscussionBoardForDelivery '''
+    cnx = connect_to_db()
+    cur = get_cursor(cnx)
+    cur.execute("SELECT * FROM DiscussionBoardForDishes")
+    dbfd = cur.fetchall()
+    cur.execute("SELECT * FROM DiscussionBoardForChefs")
+    dbfc = cur.fetchall()
+    cur.execute("SELECT * FROM DiscussionBoardForDelivery")
+    dbfdl = cur.fetchall()
+    print("Dishes Discussion Board:")
+    for x in dbfd:
+        print(x)
+    print("Chefs Discussion Board:")
+    for x in dbfc:
+        print(x)
+    print("Delivery Discussion Board:")
+    for x in dbfdl:
+        print(x)
+    cur.close()
+    cnx.close()
