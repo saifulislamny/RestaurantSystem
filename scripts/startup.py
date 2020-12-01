58a8b0a1563704cadc80967a56519644c8b8339f
Saiful Islam <nysaifulislam@gmail.com>
2020-12-01 18:54:17 -0500

    revisions made from my last push

--- scripts/startup.py
+++ scripts/startup.py
@@ -2,6 +2,12 @@
 
 import tkinter as tk
 
+# TODO: Daniel, specify that you and I worked on this file in the header (you had it when you gave me your first ever code, which I should have kept, my mistake)
+# TODO: Daniel, you forgot to import functions here as well
+# TODO: Daniel, if you have already checked to see that these functions work properly, then ignore this comment. Otherwise, check to see if these functions work properly through a separate file on your machine.
+# TODO: Daniel, check these errors that I get with VSCode
+# TODO: Daniel, remove TODOs that you have already completed (leave them if you haven't completed yet)
+
 def verify_login_details(username, password): # TODO: Daniel, implement this function
     ''' Returns true/false if the login information is registered on the system by checking Accounts table ''' 
     cnx = connect_to_db()
