9011e1e4e093618ec2763927bca93f0a4074b23a
Daniel Lichter <danlichter096@gmail.com>
2020-12-03 22:41:06 -0500

    Update manager_operations.py

--- scripts/manager_operations.py
+++ scripts/manager_operations.py
@@ -1,14 +1,10 @@
 # This file is used to handle operations which managers are allowed to do
-
+''' @authors: daniellichter, saifulislam '''
 from account_management import delete_account_as_manager
-
-# TODO: Daniel, specify that you and I worked on this file in the header (you had it when you gave me your first ever code, which I should have kept, my mistake)
-# TODO: Daniel, you forgot to import functions here as well
+from db_handling import connect_to_db, get_cursor, save_db_changes, close_db
 # TODO: Daniel, if you have already checked to see that these functions work properly, then ignore this comment. Otherwise, check to see if these functions work properly through a separate file on your machine.
-# TODO: Daniel, use proper naming conventions (I think everything in Python is snake_case except for classes (Pascal), exceptions (Pascal), constants (CAPS_WITH_UNDER), global constants (CAPS_WITH_UNDER))
-# TODO: Daniel, check these errors that I get with VSCode
-# TODO: Daniel, make sure your indentations are correct
-# TODO: Daniel, I get an error when I run python3 on this file
+
+
 
 def accept_customer_registrations(username):
     '''
@@ -21,23 +17,23 @@ def accept_customer_registrations(username):
     if(len(username <=15)):
         cur.execute("SELECT cust_username FROM CustomerRegistrations")
         cust_reg = cur.fetchall()
-        custReg = []
+        cr_list = []
         for [x] in cust_reg:
-            cust.append(x)
-        if(username not in custReg):
+            cr_list.append(x)
+        if(username not in cr_list):
             return False
     # otherwise:
     # add username, password, and "RC" (stands for "Registered Customer") to Accounts table by taking info from CustomerRegistrations table
         cur.execute("SELECT password, amt_of_deposit FROM CustomerRegistrations where cust_username = '%s'" %username)
         cust_inf = cur.fetchall()
-        custInfo = []
+        ci_list = []
         for x in cust_inf:
-            custInfo.append(x)
-        cur.execute("INSERT INTO Accounts(username, password, type) VALUES (%s,%s,'RC')", (username, custInfo[0][0]))
+            ci_list.append(x)
+        cur.execute("INSERT INTO Accounts(username, password, type) VALUES (%s,%s,'RC')", (username, ci_list[0][0]))
     # add username, amount of deposit, number of warnings (set to 0 since a new account) to CustomerAccounts table by taking info from CustomerRegistrations table
-        cur.execute("INSERT INTO CustomerAccounts(username, amt_of_deposit) VALUES (%s,%s)", (username, custInfo[0][1]))
+        cur.execute("INSERT INTO CustomerAccounts(username, amt_of_deposit) VALUES (%s,%s)", (username, ci_list[0][1]))
     # delete row from CustomerRegistrations table that matches username
-        cur.execute("DELETE FROM CustomerRegistrations WHERE cust_username = %s" %username))
+        cur.execute("DELETE FROM CustomerRegistrations WHERE cust_username = %s" %username)
         save_db_changes(cur,cnx)
         return True
     else:
@@ -56,10 +52,8 @@ def close_customer_account(username):
     # for this function you only have to check if the username exists in the Accounts table AS A CUSTOMER and return false if not, everything else should be fine
         cur.execute("SELECT username FROM Accounts WHERE username = '%s' AND type in ('RC','VC')" %username)
         acc = cur.fetchone()[0]
-        if(len(a)==0):
+        if(len(acc)==0):
             return False
-
-
         if delete_account_as_manager(username):
             return True
         else:
@@ -123,17 +117,19 @@ def decline_customer_registrations(username):
     Output: Returns true/false if row in CustomerRegistrations table associated with username is deleted successfully
     '''
     # if username does not exist in the CustomerRegistrations table, return false
-        cnx = connect_to_db()
-        cur = get_cursor(cnx)
-        if(len(username <= 15)):
-            cur.execute("SELECT cust_username FROM CustomerRegistrations WHERE cust_username = '%s'" %username)
-            acc = cur.fetchall()
-            if(len(acc)==0):
-                return False
-            else:
-                cur.execute("DELETE FROM CustomerRegistrations WHERE cust_username = %s" %username))
-                save_db_changes(cur,cnx)
-                return True
+    cnx = connect_to_db()
+    cur = get_cursor(cnx)
+    if(len(username <= 15)):
+        cur.execute("SELECT cust_username FROM CustomerRegistrations WHERE cust_username = '%s'" %username)
+        acc = cur.fetchall()
+        if(len(acc)==0):
+            return False
+        else:
+            cur.execute("DELETE FROM CustomerRegistrations WHERE cust_username = %s" %username)
+            save_db_changes(cur,cnx)
+            return True
+    else:
+        return False
 
 
     # if so, delete row in CustomerRegistrations and return true
@@ -165,17 +161,46 @@ def raise_employee_pay(username, increment):
     else:
         return False
 
-def view_chef_complaints_and_compliments(): # TODO: Daniel, implement this function
+def view_chef_complaints_and_compliments(): 
     ''' Output: Returns a string of all entries in the ChefComplaintsAndCompliments table '''
+    if(len(username)>15 or len(username)==0):
+        return False
+    cnx = connect_to_db()
+    cur = get_cursor(cnx)
+    cur.execute("SELECT * FROM ChefComplaintsandCompliments")
+    chef_complaints = cur.fetchall()
+    cc_list = []
+    cc_str = ''
+    for x in chef_complaints:
+        cc_list.append(x)
+    for x in mr_list:
+        cc_str += x
+    return cc_str
 
-def view_delivery_complaints_and_compliments(): # TODO, Daniel, implement this function
+def view_delivery_complaints_and_compliments(): 
     ''' Output: Returns a string of all entries in the DeliveryComplaintsAndCompliments table '''
+    if(len(username)>15 or len(username)==0):
+        return False
+    cnx = connect_to_db()
+    cur = get_cursor(cnx)
+    cur.execute("SELECT * FROM DeliveryComplaintsandCompliments")
+    delivery_complaints = cur.fetchall()
+    dc_list = []
+    dc_str = ''
+    for x in delivery_complaints:
+        dc_list.append(x)
+    for x in mr_list:
+        dc_str += x
+    return dc_str
 
 def view_customer_registrations():
     ''' Output: Returns a string of all usernames and deposit information in the CustomerRegistrations table '''
     cnx = connect_to_db()
     cur = get_cursor(cnx)
     cur.execute("SELECT cust_username, amt_of_deposit FROM CustomerRegistrations")
-    custReg = cur.fetchall()
-    for x in custReg:
-        print(x)
+    cust_reg = cur.fetchall()
+    cr_str = ''
+    for x in cust_reg:
+        cr_str += x
+    return cr_str
+        
