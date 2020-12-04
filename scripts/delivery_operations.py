09dc2348884958606b79c495944434bd2c3e83a8
Daniel Lichter <danlichter096@gmail.com>
2020-12-03 22:29:57 -0500

    Update delivery_operations.py

--- scripts/delivery_operations.py
+++ scripts/delivery_operations.py
@@ -1,12 +1,11 @@
 # This file is used to handle operations which delivery people are allowed to do
-
-# TODO: Daniel, specify that you and I worked on this file in the header (you had it when you gave me your first ever code, which I should have kept, my mistake)
-# TODO: Daniel, you forgot to import the functions here as well
+''' @authors: daniellichter, saifulislam '''
+from db_handling import connect_to_db, get_cursor, save_db_changes, close_db
 # TODO: Daniel, if you have already checked to see that these functions work properly, then ignore this comment. Otherwise, check to see if these functions work properly through a separate file on your machine.
 # TODO: Daniel, use proper naming conventions (I think everything in Python is snake_case except for classes (Pascal), exceptions (Pascal), constants (CAPS_WITH_UNDER), global constants (CAPS_WITH_UNDER))
-# TODO: Daniel, check these errors that I get with VSCode
-# TODO: Daniel, make sure your indentations are correct
-# TODO: Daniel, I get an error when I run python3 on this file
+
+
+
 
 def complete_delivery(delivery_order_id):
     '''
@@ -16,16 +15,16 @@ def complete_delivery(delivery_order_id):
     # if delivery_order_id does not exist in the Deliveries table, return false
     cnx = connect_to_db()
     cur = get_cursor(cnx)
-    delOrd = []
+    do_list = []
     cur.execute("SELECT delivery_order_num FROM Deliveries")
     del_ord = cur.fetchall()
     for [x] in del_ord:
-        delOrd.append(x)
-    if(delivery_order_id not in delOrd):
+        do_list.append(x)
+    if(delivery_order_id not in do_list):
         return False
     # otherwise perform actions and return true
     else:
-        cur.execute("DELETE FROM Deliveries WHERE delivery_order_num = %s" %delivery_order_id))
+        cur.execute("DELETE FROM Deliveries WHERE delivery_order_num = %s" %delivery_order_id)
         save_db_changes(cur,cnx)
         return True
 
@@ -35,14 +34,32 @@ def view_deliveries():
     cur = get_cursor(cnx)
     cur.execute("SELECT * FROM Deliveries")
     deliv = cur.fetchall()
+    deliv_list = []
+    deliv_str = ''
     for x in deliv:
-        print(x)
+        deliv_list.append(x)
+    for x in deliv_list:
+        deliv_str += x
     cur.close()
     cnx.close()
+    return deliv_str
 
-def view_ratings_of_delivery_person(username): # TODO: Daniel, implement this function
+def view_ratings_of_delivery_person(username):
     '''
     username: username of delivery person
     Output: Returns average rating of delivery person for all deliveries they made
     '''
     # use DeliveryVotes table
+    if(len(username)>15 or len(username)==0):
+        return False
+    cnx = connect_to_db()
+    cur = get_cursor(cnx)
+    cur.execute("SELECT delivery_username, rating FROM DeliveryVotes")
+    deliv_rating = cur.fetchall()
+    deliv_list = []
+    deliv_str = ''
+    for x in deliv_rating:
+        deliv_list.append(x)
+    for x in deliv_list:
+        deliv_str += x
+    return deliv_str
