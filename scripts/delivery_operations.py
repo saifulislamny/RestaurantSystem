55725f5be75e5de240f75f0d281069a757da1896
Daniel Lichter <danlichter096@gmail.com>
2020-12-09 01:20:29 -0500

    Update delivery_operations.py

--- scripts/delivery_operations.py
+++ scripts/delivery_operations.py
@@ -3,10 +3,7 @@
 from db_handling import connect_to_db, get_cursor, save_db_changes, close_db
 
 
-
-
-
-def complete_delivery(username, delivery_order_id): # TODO: Daniel, implement this function (modification made)
+def complete_delivery(username, delivery_order_id):
     '''
     delivery_order_id: ID number of the delivery order (not guaranteed to match conditions)
     Output: Returns true/false if modifies row in Deliveries table
@@ -22,15 +19,14 @@ def complete_delivery(username, delivery_order_id): # TODO: Daniel, implement th
     if(delivery_order_id not in do_list):
         return False
     # TODO: Daniel, make sure delivery is not already completed by someone else (return false if it is)
-
+    cur.execute("SELECT cust_username FROM Deliveries WHERE delivery_order_num = '%s' and delivery_username IS NULL" %delivery_order_id)
+    deliv = cur.fetchone()[0]
+    if(len(deliv)==0):
+        return False
     # otherwise perform actions and return true
-    else:
-
-        # we don't delete anymore so this line is unnecessary# cur.execute("DELETE FROM Deliveries WHERE delivery_order_num = %s" %delivery_order_id)
-        # TODO: Daniel, modify row in Deliveries table by updating the delivery username in the row that matches the delivery_order_id (the delivery username entity will originally be empty)
-
-        save_db_changes(cur,cnx)
-        return True
+    cur.execute("UPDATE Deliveries Set delivery_username = %s WHERE delivery_order_num = %s",(username, delivery_order_id))
+    save_db_changes(cur,cnx)
+    return True
 
 def view_deliveries():
     ''' Output: Returns string of all deliveries in the Deliveries table '''
@@ -48,9 +44,17 @@ def view_deliveries():
     cnx.close()
     return deliv_str
 
-def view_incomplete_deliveries(): # TODO: Daniel, implement this function
+def view_incomplete_deliveries(): 
     ''' Output: Returns string of all incomplete deliveries in the Deliveries table '''
     # essentially, all rows where the delivery person column is NULL/empty 
+    cnx = connect_to_db()
+    cur = get_cursor(cnx)
+    cur.execute("SELECT * FROM Deliveries WHERE delivery_username IS NULL")
+    inc_deliv = cur.fetchall()
+    id_str = ''
+    for x in inc_deliv:
+        id_str += (str(x[0])+" "+x[1]+" "+x[2]+" "+x[3]+"\n")
+    return id_str
 
 def view_ratings_of_delivery_person(username):
     '''
@@ -72,5 +76,3 @@ def view_ratings_of_delivery_person(username):
         avg+= x[1]
     avg/=len(deliv_list)
     return avg
-    
-print(view_ratings_of_delivery_person('littleCub18'))
